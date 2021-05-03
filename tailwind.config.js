7288e2b4095f97a44f7b5ba4ff85723742b33c4a
Amy Grouse <amygrouse@Amys-MacBook-Pro-5.local>
2021-05-03 19:55:54 -0400

    hamburger menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,46 +1,46 @@
 module.exports = {
-    theme: {
-      backgroundPosition: {
-        'bg-image-y': 'center top 1rem'
-      },
-        extend: {
-          transitionProperty: {
-              'width': 'width',
-              'height': 'height'
-          },
-          colors: {
-            pud_green: '#477A7C',
-            pud_gray: '#E7E7E7'
-          },
-          fontFamily: {
-            lato: ["Lato"]
-          },
-          height: theme =>({
-            "screen-3/5": "60vh",
-            "screen-3/4": "75vh",
-            "screen-4/5": "90vh",
-            "screen/2": "50vh",
-            "screen/3": "calc(100vh / 3)",
-            "screen/4": "calc(100vh / 4)",
-            "screen/5": "calc(100vh / 5)",
-          }),
-          transitionTimingFunction: {
-            'search-in-out': 'cubic-bezier(0.18, 0.89, 0.32, 1.28)',
-            'in-expo': 'cubic-bezier(0.95, 0.05, 0.795, 0.035)',
-            'out-expo': 'cubic-bezier(0.19, 1, 0.22, 1)',
-           },
-           backgroundImage: theme => ({
-            'car-1': "url('../images/hero.png')",
-           })
-        },
-
-    },
-    variants: {
-      extend: {
-        width: ['focus-within', 'hover', 'focus'],
-        padding: ['focus'],
-        transitionTimingFunction: ['hover', 'focus'],
-        transform: ['hover', 'focus'],
-      }
-    },
-}
+	theme: {
+		backgroundPosition: {
+			'bg-image-y': 'center top 1rem',
+		},
+		extend: {
+			transitionProperty: {
+				width: 'width',
+				height: 'height',
+			},
+			colors: {
+				pud_green: '#477A7C',
+				pud_gray: '#E7E7E7',
+				pud_green_sec: '#DEF2F1',
+			},
+			fontFamily: {
+				lato: ['Lato'],
+			},
+			height: (theme) => ({
+				'screen-3/5': '60vh',
+				'screen-3/4': '75vh',
+				'screen-4/5': '90vh',
+				'screen/2': '50vh',
+				'screen/3': 'calc(100vh / 3)',
+				'screen/4': 'calc(100vh / 4)',
+				'screen/5': 'calc(100vh / 5)',
+			}),
+			transitionTimingFunction: {
+				'search-in-out': 'cubic-bezier(0.18, 0.89, 0.32, 1.28)',
+				'in-expo': 'cubic-bezier(0.95, 0.05, 0.795, 0.035)',
+				'out-expo': 'cubic-bezier(0.19, 1, 0.22, 1)',
+			},
+			backgroundImage: (theme) => ({
+				'car-1': "url('../images/hero.png')",
+			}),
+		},
+	},
+	variants: {
+		extend: {
+			width: ['focus-within', 'hover', 'focus'],
+			padding: ['focus'],
+			transitionTimingFunction: ['hover', 'focus'],
+			transform: ['hover', 'focus'],
+		},
+	},
+};
