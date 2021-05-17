f11da0fee6dd7d46b5ec3fcb35ae0c18e449d688
Jacob Crabtree <jacob.crabtree@tsgforce.com>
2021-05-17 13:27:50 -0700

    Fixed webpack not applying to website and businessDirectory not displaying by finding a way to automatically determine the build/assets folders. Jacob Crabtree

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const fs = require('fs'); 
+const os = require('os');
+
+var out_path = path.resolve(__dirname, 'build/assets')
+if (!fs.existsSync(out_path)) {
+    out_path = path.resolve(os.homedir(), 'prosperusdemo.xyz/build/assets')
+}
+
 module.exports = {
   entry: './src/scripts/main.js',
   output: {
-    path: path.resolve(__dirname, 'build/assets'),
+    path: out_path,
     filename: 'main.js'
   },
   plugins: [new MiniCssExtractPlugin()],
